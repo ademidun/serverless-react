e4a56397fe4be3cd29976160a1a87837f112d9df
ademidun <tomiademidun@gmail.com>
2018-04-07 17:08:18 -0400

    added log in route and template

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "serverless-react-notes"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://4880cj9oo5.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_RaPnEaaxf",
+        APP_CLIENT_ID: "9fa5ahj1knibe6ng3fqfvqi6d",
+        IDENTITY_POOL_ID: "us-east-1:c0c0824a-24cc-4a8e-ad51-daee90caed30"
+    }
+};
